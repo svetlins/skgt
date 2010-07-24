996f1617071f27b7555b5fb442538985c7390db6
Svetlin Simonyan <svetlins@gmail.com>
2010-07-24 01:24:25 -0400

    pretty far - redesign needed

--- skgt.rb
+++ skgt.rb
@@ -3,19 +3,90 @@ require 'nokogiri'
 
 SKGT = URI.parse 'http://gps.skgt-bg.com/Web/SelectByLine.aspx'
 
-def get_some_info
+TRANSPORT_TYPE = 1 # by bus
+
+def get_junk page
+
+    viewstate = page.xpath "//input[@name='__VIEWSTATE']"
+    ev_validation = page.xpath "//input[@name='__EVENTVALIDATION']"
+
+    return viewstate.attribute('value').to_s, ev_validation.attribute('value').to_s
+end
+
+def get_initial
+    viewstate = nil
+    ev_validation = nil
+
     Net::HTTP.start(SKGT.host, SKGT.port) do |http|
-        page_html = http.get(SKGT.path)
+        response = http.get(SKGT.path)
+        page = Nokogiri::HTML response.body
 
-        page = Nokogiri::HTML page_html.body
+        viewstate, ev_validation = get_junk page
+    end
 
-        viewstate = page.xpath "//input[@name='__VIEWSTATE']"
-        ev_validation = page.xpath "//input[@name='__EVENTVALIDATION']"
+    return viewstate, ev_validation
+end
 
-        puts viewstate.attribute 'value'
-        puts
-        puts ev_validation.attribute 'value'
+def get_lines viewstate, ev_validation, transport_type
+
+    lines = {}
+    vs = nil
+    ev = nil
+
+    Net::HTTP.start(SKGT.host, SKGT.port) do |http|
+        request = Net::HTTP::Post.new SKGT.path
+        request.set_form_data '__VIEWSTATE' => viewstate,
+            '__EVENTVALIDATION' => ev_validation,
+            'ctl00$ScriptManager1' => 'ctl00$ContentPlaceHolder1$upMain|ctl00$ContentPlaceHolder1$ddlTransportType',
+            '__EVENTTARGET' => 'ctl00$ContentPlaceHolder1$ddlTransportType',
+            '__EVENTARGUMENT' => '',
+            '__LASTFOCUS' => '',
+            'ctl00$ContentPlaceHolder1$ddlTransportType' => transport_type,
+            'ctl00$ContentPlaceHolder1$ddlLines' => '',
+            'ctl00$ContentPlaceHolder1$ddlStops' => ''
+
+        response = Net::HTTP.new(SKGT.host, SKGT.port).start do |http|
+            http.request request
+        end
+
+        page = Nokogiri::HTML response.body
+
+        vs, ev = get_junk page
+
+        page.xpath('//select[@name=\'ctl00$ContentPlaceHolder1$ddlLines\']/option').each do |option|
+            
+            lines[option.children.to_s] = option.attribute('value').to_s if option.attribute('value').to_s.length > 0
+        end
+    end
+
+    return vs, ev, lines
+end
+
+def get_routes viewstate, ev_validation, transport_type, line_id
+
+    routes = {}
+
+    Net::HTTP.start(SKGT.host, SKGT.port) do |http|
+        request = Net::HTTP::Post.new SKGT.path
+        request.set_form_data '__VIEWSTATE' => viewstate,
+            '__EVENTVALIDATION' => ev_validation,
+            'ctl00$ScriptManager1' => 'ctl00$ContentPlaceHolder1$upMain|ctl00$ContentPlaceHolder1$ddlLines',
+            '__EVENTTARGET' => 'ctl00$ContentPlaceHolder1$ddlLines',
+            '__EVENTARGUMENT' => '',
+            '__LASTFOCUS' => '',
+            'ctl00$ContentPlaceHolder1$ddlTransportType' => transport_type,
+            'ctl00$ContentPlaceHolder1$ddlLines' => line_id,
+            'ctl00$ContentPlaceHolder1$ddlStops' => ''
+
+        response = Net::HTTP.new(SKGT.host, SKGT.port).start do |http|
+            http.request request
+        end
+
+        puts response.body
+        
     end
 end
 
-get_some_info
+vs, ev_validation = get_initial
+vs, ev_validation, lines = get_lines vs, ev_validation, TRANSPORT_TYPE
+puts get_routes vs, ev_validation, 1, 66
