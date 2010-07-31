80b393afeb882584b5b0e62fe8a7d0282438d488
svetlins <svetlin.s@gmail.com>
2010-07-31 16:40:34 -0400

    clarified a bit caching scheme
    (which is not really a caching scheme)

--- web.rb
+++ web.rb
@@ -4,9 +4,9 @@ require 'skgt'
 require 'json'
 
 
-get '/cache/' do
+get '/initial_data/' do
     content_type 'text/json', :charset => 'utf-8'
-    (File.open 'cache').read
+    (File.open 'initial_data').read
 end
     
 
@@ -14,34 +14,7 @@ get'/' do
     redirect '/main.html'
 end
 
-get '/lines/' do
-    content_type 'text/json', :charset => 'utf-8'
-    skgt = Skgt::SKGTHandler.new
-    lines = skgt.get_lines params[:ttype]
-
-    JSON.dump(lines)
-end
-
-get '/routes/' do
-    content_type 'text/json', :charset => 'utf-8'
-    skgt = Skgt::SKGTHandler.new
-    routes = skgt.get_routes params[:ttype], params[:line]
-
-    JSON.dump(routes)
-end
-
-get '/stops/' do
-    content_type 'text/json', :charset => 'utf-8'
-    skgt = Skgt::SKGTHandler.new
-    stops = skgt.get_stops params[:ttype], params[:line], params[:route]
-
-    JSON.dump(stops)
-end
-
 get '/times/' do
-    # for testing purposes
-    # return JSON.dump((0..7).map { |x| '22:%02d' % (5 * x) })
-
     content_type 'text/json', :charset => 'utf-8'
     skgt = Skgt::SKGTHandler.new
     times = skgt.get_times params[:ttype], params[:line], params[:route], params[:stop]
@@ -51,5 +24,4 @@ get '/times/' do
     end
 
     JSON.dump(times)
-
 end
